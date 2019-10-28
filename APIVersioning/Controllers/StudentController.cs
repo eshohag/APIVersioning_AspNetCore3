a82d65432837902ed47dbb78684ddd86b94c9620
Md. Shohag Mia <shohag.mia@datasoft-bd.com>
2019-10-28 13:41:43 +0600

    Add project files.

---
+++ APIVersioning/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using UMS.Entities.Students;
+
+namespace APIVersioning.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [ApiVersion("3.3")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [MapToApiVersion("1.0")]
+        [HttpGet]
+        public IEnumerable<Student> Student1()
+        {
+            return new List<Student>() {
+                new Student { Id = 1, CreatedDate = DateTime.Now, CreatedUserId = 101, DepartmentId = 1, EnrollmentDate = DateTime.Now, FullName = "Shohag", IsActive = true, ModifiedDate = DateTime.Now, ModifiedUserId = 101 },
+            };
+        }
+
+        [MapToApiVersion("2.0")]
+        [HttpGet]
+        public IEnumerable<Student> Student2()
+        {
+            return new List<Student>() {
+                new Student { Id = 1, CreatedDate = DateTime.Now, CreatedUserId = 101, DepartmentId = 1, EnrollmentDate = DateTime.Now, FullName = "Shohag", IsActive = true, ModifiedDate = DateTime.Now, ModifiedUserId = 101 },
+                new Student { Id = 2, CreatedDate = DateTime.Now, CreatedUserId = 101, DepartmentId = 1, EnrollmentDate = DateTime.Now, FullName = "Arif", IsActive = true, ModifiedDate = DateTime.Now, ModifiedUserId = 101 },
+            };
+        }
+        [MapToApiVersion("3.3")]
+        [HttpGet]
+        public IEnumerable<Student> Student3()
+        {
+            return new List<Student>() {
+                new Student { Id = 1, CreatedDate = DateTime.Now, CreatedUserId = 101, DepartmentId = 1, EnrollmentDate = DateTime.Now, FullName = "Shohag", IsActive = true, ModifiedDate = DateTime.Now, ModifiedUserId = 101 },
+                new Student { Id = 2, CreatedDate = DateTime.Now, CreatedUserId = 101, DepartmentId = 1, EnrollmentDate = DateTime.Now, FullName = "Arif", IsActive = true, ModifiedDate = DateTime.Now, ModifiedUserId = 101 },
+                new Student { Id = 3, CreatedDate = DateTime.Now, CreatedUserId = 101, DepartmentId = 1, EnrollmentDate = DateTime.Now, FullName = "Milon", IsActive = true, ModifiedDate = DateTime.Now, ModifiedUserId = 101 },
+            };
+        }
+    }
+}
---
+++ APIVersioning/Startup.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace APIVersioning
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddMvc();
+            services.AddMvcCore();
+            services.AddApiVersioning(o =>
+            {
+                o.ReportApiVersions = true;
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
